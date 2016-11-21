9dde0ff04089ff54213b66a3196bf6285fc870c1
Tymoteusz Cejrowski <tymoteusz.cejrowski@gmail.com>
2016-11-21 21:56:55 +0100

    Initial commit.

---
+++ fft_serial.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+
+
+int main(int argc, char const *argv[]) {
+
+  printf(" ********************************* \n");
+  printf(" *** FFT - serial implentation *** \n");
+  printf(" ********************************* \n");
+  printf(" ***  T.Cejrowski 21.11.2016   *** \n");
+  printf(" ********************************* \n");
+  printf(" \n\n");
+
+  return 0;
+}
