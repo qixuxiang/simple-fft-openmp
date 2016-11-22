563afacf65a2a40e655c3e5cebce16e21f5ab33c
Tymoteusz Cejrowski <tymoteusz.cejrowski@gmail.com>
2016-11-23 00:19:03 +0100

    Added samples generation.

--- fft_omp.c
+++ fft_omp.c
@@ -6,26 +6,26 @@
 
 #include "fft_utils.h"
 
-#define DEBUG
+//#define DEBUG
 
-void init_W_table(double complex *W, int W_size);
+void fftOmp_initWtable(double complex *W, int W_size);
+double fftOmp_randomGen (double *seed);
 
-
-void fft(double complex* array, int array_size)
+void fft(double complex* array, unsigned long array_size)
 {
   double complex *W;
-  int n = 1, i;
-  int a = array_size / 2;
+  unsigned long n = 1, i;
+  unsigned long a = array_size / 2;
 
   // Alocate and init W
   W = (double complex*) malloc( array_size/2 * sizeof(complex double));
-  init_W_table(W, array_size);
+  fftOmp_initWtable(W, array_size);
 
   // Bits Inversion
-  reverse_array(array, 8);
+  fftUtils_reverseArray(array, 8);
 
   // For through stages
-  for(int j = 0; j < log2(array_size); j++) {
+  for(unsigned long j = 0; j < log2(array_size); j++) {
   // Main loop paralelization
  #pragma omp parallel shared ( array, array_size, W, n, a ) private (i)
  #pragma omp for nowait
@@ -48,15 +48,22 @@ void fft(double complex* array, int array_size)
     }
   #endif
 
+  free(W);
 }
 
 int main(int argc, char const *argv[]) {
 
   double complex samples[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+  double complex *input;
+
+  int exponent;
+  unsigned long n;
+
+  static double seed;
   int nThreads, tid;
 
   printf(" ********************************* \n");
-  printf(" *** FFT - serial implentation *** \n");
+  printf(" *** FFT - OMP implentation  ***** \n");
   printf(" ********************************* \n");
   printf(" ***  T.Cejrowski 21.11.2016   *** \n");
   printf(" ********************************* \n");
@@ -65,13 +72,47 @@ int main(int argc, char const *argv[]) {
   printf ( "  Number of threads =              %d\n", omp_get_max_threads ( ) );
   printf ( "\n");
 
-  fft(samples, 8);
+  printf ( "  Preparing for samples generation... Enter n (2^n) : \n" );
+  scanf ( "\t %i", &exponent);
+  n = pow(2, exponent);
+  printf ( "  Generating %lu (2^%i) samples... \n\n",  n, exponent);
+
+  seed  = 321.0;
+
+  input = ( double complex * ) malloc ( n * sizeof ( double complex) );
+  // Initialize data
+  for (int i = 0; i < n; i++ )
+  {
+    input[i] = fftOmp_randomGen(&seed);
+    input[i] += fftOmp_randomGen(&seed)*I;
+  }
+  printf(" Done! \n");
+  // Main FFT function
+  fft(input, n);
 
   printf(" Success! \n");
+  free(input);
+
   return 0;
 }
+/******************************************************************************/
 
-void init_W_table(double complex *W, int number_of_samples)
+double fftOmp_randomGen (double *seed)
+{
+  double d2 = 0.2147483647e10;
+  double t;
+  double value;
+
+  t = ( double ) *seed;
+  t = fmod ( 16807.0 * t, d2 );
+  *seed = ( double ) t;
+  value = ( double ) ( ( t - 1.0 ) / ( d2 - 1.0 ) );
+
+  return value;
+}
+/******************************************************************************/
+
+void fftOmp_initWtable(double complex *W, int number_of_samples)
 {
   int i;
 
